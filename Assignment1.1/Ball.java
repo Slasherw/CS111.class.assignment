222362e6274ff78c46dbfa7ffc3956cb969d9ed6
Slasherw <noppakron.son@dome.tu.ac.th>
2024-02-01 01:37:37 +0700

    BallTest Finish

---
+++ Assignment1.1/Ball.java
@@ -0,0 +1,21 @@
+//นพกรณ์ สงเคราะห์
+//6609540023
+import java.lang.Math;
+public class Ball {
+    private double radius;//declare variables
+    public void setRadius(double aRadius){
+        radius = aRadius;
+    }
+    
+    public double getRadius(){
+        return radius;
+    }
+
+    public double getSurface(){
+        return 4*Math.PI*Math.pow(radius,2);
+    }
+
+    public double getVolume(){
+        return Math.PI*Math.pow(radius,3)*4/3;
+    }
+}
---
+++ Assignment1.1/BallTest.java
@@ -0,0 +1,16 @@
+//นพกรณ์ สงเคราะห์
+//6609540023
+import java.util.Scanner;
+public class BallTest {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Input the radius of the ball > ");
+        double radius = input.nextDouble();
+        
+        Ball cal = new Ball();
+        cal.setRadius(radius);
+        System.out.printf("The ball radius '%.2f' has surface area=%.3f The ball%nradius '%.2f' has volume =%.3f",cal.getRadius(),cal.getSurface(),cal.getRadius(),cal.getVolume());
+        
+        input.close();
+    }
+}
