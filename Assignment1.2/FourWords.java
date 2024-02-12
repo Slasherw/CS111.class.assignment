6690ad400aa19df8f9ac1afb2c91a1db3f6e0f25
Slasherw <noppakron.son@dome.tu.ac.th>
2024-02-12 23:31:45 +0700

    Assign1.2 method.add finish

--- Assignment1.2/FourWords.java
+++ Assignment1.2/FourWords.java
@@ -6,32 +6,135 @@ public class FourWords {
     private String word2;
     private String word3;
     private String word4;
-    private int count;//ไม่ควร set ค่าตัวแปรไว้ใน instanceVariable ควรเซทไว้ในMethods
+    private int count=0;
 
     public void add(String word){
-        count=1;//ควร Setค่าแบบนี้ที่ MethodF
-        for(int i=count;i<=4;i++){
-            if(count==1){
-                word1=word;
-            }else if(count==2){
+            switch (count) {
+                case 0:
+                    word1=word;
+                    break;
+                case 1:
                     word2=word;
-                }else if(count==3){
-                        word3=word;
-                    }else if(count==4){
-                            word4=word;
-                        }
-        }
+                    break;
+                case 2:
+                    word3=word;
+                    break;
+                case 3:
+                    word4=word;
+                    break;
+                default:
+                    System.out.println("Can't add a word "+word);
+                    break;
+            }
+            count++;
     }
+
+
     public String getMin(){
-        if(word1)
-        return 0;
+        String min=word1;
+        //compare word1
+        if(word1.compareTo(word2)<0){
+            min=word1;
+        }
+        if(word1.compareTo(word3)<0){
+            min=word1;
+        }
+        if(word1.compareTo(word4)<0){
+            min=word1;
+        }
+
+        //compare word2
+        if(word2.compareTo(word1)<0){
+            min=word2;
+        }
+        if(word2.compareTo(word3)<0){
+            min=word2;
+        }
+        if(word2.compareTo(word4)<0){
+            min=word2;
+        }
+
+        //compare word3
+        if(word3.compareTo(word1)<0){
+            min=word3;
+        }
+        if(word3.compareTo(word2)<0){
+            min=word3;
+        }
+        if(word3.compareTo(word4)<0){
+            min=word3;
+        }
+
+        //compare word4
+        if(word4.compareTo(word1)<0){
+            min=word4;
+        }
+        if(word4.compareTo(word2)<0){
+            min=word4;
+        }
+        if(word4.compareTo(word3)<0){
+            min=word4;
+        }
+        return min;
     }
 
     public String getMax(){
-        return 0;
-    }
-    
-    public void printWords(){
+        String max=word1;
+        //compare word1
+        if(word1.compareTo(word2)<0){
+            max=word1;
+        }
+        if(word1.compareTo(word3)<0){
+            max=word1;
+        }
+        if(word1.compareTo(word4)<0){
+            max=word1;
+        }
 
+        //compare word2
+        if(word2.compareTo(word1)<0){
+            max=word2;
+        }
+        if(word2.compareTo(word3)<0){
+            max=word2;
+        }
+        if(word2.compareTo(word4)<0){
+            max=word2;
+        }
+
+        //compare word3
+        if(word3.compareTo(word1)<0){
+            max=word3;
+        }
+        if(word3.compareTo(word2)<0){
+            max=word3;
+        }
+        if(word3.compareTo(word4)<0){
+            max=word3;
+        }
+
+        //compare word4
+        if(word4.compareTo(word1)<0){
+            max=word4;
+        }
+        if(word4.compareTo(word2)<0){
+            max=word4;
+        }
+        if(word4.compareTo(word3)<0){
+            max=word4;
+        }
+        return max;
+    }
+
+    public void printWords(){
+        System.out.println(" List of words:");
+        System.out.println("\t"+word1);
+        System.out.println("\t"+word2);
+        System.out.println("\t"+word3);
+        System.out.println("\t"+word4);
+        System.out.println("The lexicographic minimum is "+getMin());
+        System.out.println("The lexicographic maximum is "+getMax());
+        System.out.println("Total length "+(word1.length()+word2.length()+word3.length()+word4.length()));
+        System.out.println("Average lenght "+((word1.length()+word2.length()+word3.length()+word4.length())/4.00));
     }
 }
---
+++ Assignment1.2/FourWordsTest.java
@@ -0,0 +1,15 @@
+//นพกรณ์ สงเคราะห์
+//6609540023
+import java.util.Scanner;
+public class FourWordsTest {
+
+    public static void main(String[] args){
+        FourWords words4 = new FourWords();
+        words4.add("cash");
+        words4.add("apple");
+        words4.add("trees");
+        words4.add("bee");
+        words4.add("something");
+        words4.printWords();
+    }
+}
