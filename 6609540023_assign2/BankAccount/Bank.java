e8495c004cf9b280e3a67ac0b7013a282f1adcbd
Slasherw <noppakron.son@dome.tu.ac.th>
2024-03-26 01:24:28 +0700

    Account finish Bank still confuse in foreach

---
+++ 6609540023_assign2/BankAccount/Bank.java
@@ -0,0 +1,45 @@
+package BankAccount;
+
+import java.util.ArrayList;
+
+public class Bank {
+    private ArrayList<Account> accountList;
+
+    public void addAccount(Account acct){//add object to accountList
+        accountList.add(acct);
+    }
+
+    public Account getAccount(int index){//return index in Account
+        return accountList.get(index);
+    }
+
+    public ArrayList<Account> findAccounts(double amount){
+        //return ArrayList that have money in Account >= amount 
+        //if ArrayList is not found return null
+        for (Account money : accountList) {
+            if(money>=amount)
+        }
+    }
+
+    public Account findMin(){
+        //return null if no account found
+        //else return Account that has least amount of money in accountList
+    }
+
+    public Account findMax(){
+        //return null if no account found
+        //else return Account that has most amount of money in accountList
+    }
+
+    public void addInterest(){
+        //increase interest 3% in each account in accountList
+    }
+
+    public double getTotal(){
+        //find the sum of money in accountList
+    }
+
+    public double getAverage(){
+        //find the average of money in accountList
+    }
+}
