7cd9568063350e7544d33e92ab3b29020178512b
Slasherw <noppakron.son@dome.tu.ac.th>
2024-03-31 14:33:24 +0700

    Phone bills fix loop

--- 6609540023_assign2/PhoneBill.java
+++ 6609540023_assign2/PhoneBill.java
@@ -6,35 +6,38 @@ public class PhoneBill {
     public static void main(String[] args) {
 
         Scanner input = new Scanner(System.in);
-        for(int i=1;i>0;i++){
-        System.out.print("Enter minutes: ");
-        double time = input.nextDouble();
-        double minute = Math.ceil(time);
-        if(minute <= 0){
-            System.out.println("You have to pay 0.0");
-        }
-        
-        if(minute > 0 && minute < 10){
-            System.out.printf("You have to pay "+ minute*3.50 + "\n");
-        }
-        
-        if(minute >= 10 && minute < 60){
-            if(minute <= 20){
-                System.out.printf("You have to pay "+ minute*3 + "\n");
+        boolean i ;
+        while ( i = true) {
+            System.out.print("Enter minutes (Q = Quit): ");
+            String time = input.nextLine();
+            double timeChange = Double.parseDouble(time);
+            double minute = Math.ceil(timeChange);
+            if(minute <= 0){
+                System.out.println("You have to pay 0.0");
             }
+        
+            if(minute > 0 && minute < 10){
+                System.out.printf("You have to pay "+ minute*3.50 + "\n");
+            }
+        
+            if(minute >= 10 && minute < 60){
+                if(minute <= 20){
+                    System.out.printf("You have to pay "+ minute*3 + "\n");
+                }
 
-            if(minute > 20 && minute < 60){
-                System.out.printf("You have to pay " + ((20*3) + ((minute - 20)*1)) + "\n");
+                if(minute > 20 && minute < 60){
+                    System.out.printf("You have to pay " + ((20*3) + ((minute - 20)*1)) + "\n");
+                }
             }
-        }
 
             if(minute >= 60){
                 System.out.printf("You have to pay " + (minute * 1.5) + "\n");
             }
 
-        if(time == 0){
-            i=0;
-        }
-    }
+            if(time == "Q"){
+                i=false;
+                
+            }
+        }input.close();
     }
 }
