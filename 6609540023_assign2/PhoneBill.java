d432343177ad14bb9bb4d3f51c46f97ef5287a8d
Slasherw <noppakron.son@dome.tu.ac.th>
2024-04-01 01:12:26 +0700

    PhoneBills fix Q input

--- 6609540023_assign2/PhoneBill.java
+++ 6609540023_assign2/PhoneBill.java
@@ -6,37 +6,40 @@ public class PhoneBill {
     public static void main(String[] args) {
 
         Scanner input = new Scanner(System.in);
-        boolean i ;
-        while ( i = true) {
+        boolean loop ;
+        while ( loop = true) {
             System.out.print("Enter minutes (Q = Quit): ");
             String time = input.nextLine();
-            double timeChange = Double.parseDouble(time);
-            double minute = Math.ceil(timeChange);
-            if(minute <= 0){
-                System.out.println("You have to pay 0.0");
-            }
-        
-            if(minute > 0 && minute < 10){
-                System.out.printf("You have to pay "+ minute*3.50 + "\n");
-            }
-        
-            if(minute >= 10 && minute < 60){
-                if(minute <= 20){
-                    System.out.printf("You have to pay "+ minute*3 + "\n");
-                }
-
-                if(minute > 20 && minute < 60){
-                    System.out.printf("You have to pay " + ((20*3) + ((minute - 20)*1)) + "\n");
-                }
-            }
-
-            if(minute >= 60){
-                System.out.printf("You have to pay " + (minute * 1.5) + "\n");
-            }
-
-            if(time == "Q"){
-                i=false;
+            if(time.equalsIgnoreCase("Q")){
+                loop = false;
                 break;
+            }
+            try{
+                double timeChange = Double.parseDouble(time);
+                double minute = Math.ceil(timeChange);
+                if(minute <= 0){
+                    System.out.println("You have to pay 0.0");
+                }
+        
+                if(minute > 0 && minute < 10){
+                    System.out.printf("You have to pay "+ minute*3.50 + "\n");
+                }
+        
+                if(minute >= 10 && minute < 60){
+                    if(minute <= 20){
+                        System.out.printf("You have to pay "+ minute*3 + "\n");
+                    }
+
+                    if(minute > 20 && minute < 60){
+                        System.out.printf("You have to pay " + ((20*3) + ((minute - 20)*1)) + "\n");
+                    }
+                }
+
+                if(minute >= 60){
+                    System.out.printf("You have to pay " + (minute * 1.5) + "\n");
+                }
+            } catch (NumberFormatException e){
+                System.out.println("Invalid input. Please enter a number or 'Q' to quit.");
             }
         }input.close();
     }
