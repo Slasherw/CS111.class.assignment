487833cd91f3cf9e52463a35d951011b32f9a4ec
Slasherw <noppakron.son@dome.tu.ac.th>
2024-04-16 16:48:24 +0700

    create Bread class

---
+++ 6609540023_assign3/Bread/Bread.java
@@ -0,0 +1,25 @@
+package 6609540023_assign3.Bread;
+
+public class Bread {
+    public static final String BURNT="Burn";
+    public static final String CRISP="Crisp";
+    public static final String SOFT="Soft";
+    private String state;
+
+    //create bread
+    public Bread(){
+        state = SOFT;
+    }
+
+    public void setState(String state){
+        this.state = state;
+    }
+
+    public String getState(){
+        return state;
+    }
+
+    public String toString(){
+        return "Bread is now: "+ state;
+    }
+}
